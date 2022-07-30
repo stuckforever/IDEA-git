0474106d7c6c6f7bb06eb87f620b5befff96f353
stuckforever <101959449+stuckforever@users.noreply.github.com>
2022-07-30 21:16:53 +0800

    pull test

--- git-demo/src/main/java/org/example/App.java
+++ git-demo/src/main/java/org/example/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println("master");
         System.out.println("push test");
         System.out.println("push test---SSH");
+        System.out.println("pull test");
     }
 }
