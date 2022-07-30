a0943792b8ed0df77609ba06aa5f87ae21ca2089
gf <2389924121@qq.com>
2022-07-30 20:32:46 +0800

    IDEA hot-fix first commit

--- git-demo/src/main/java/org/example/App.java
+++ git-demo/src/main/java/org/example/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("hello git22222222222");
+        System.out.println("hello git 33333333333333333");
     }
 }
