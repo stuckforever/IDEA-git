f54cc8a86df6af3515b2daf3ce8dba3604a1cd60
gf <2389924121@qq.com>
2022-07-30 21:13:18 +0800

    push test-SSH

--- git-demo/src/main/java/org/example/App.java
+++ git-demo/src/main/java/org/example/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("push test");
+        System.out.println("push test---SSH");
     }
 }
