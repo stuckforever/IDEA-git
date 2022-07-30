d242ae7b5036ffc92374f96c4781c7015a13a400
gf <2389924121@qq.com>
2022-07-30 20:35:59 +0800

    IDEA master second commit

--- git-demo/src/main/java/org/example/App.java
+++ git-demo/src/main/java/org/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hello git22222222222");
         System.out.println("hello git 33333333333333333");
+        System.out.println("master");
     }
 }
